8e8d088c6ef1c4e44ec1d61554d7e1c608450d97
Admin <Admin@D20929>
2015-05-23 14:25:31 +0700

    2113110009

---
+++ example01/src/example01/Main.java
@@ -0,0 +1,16 @@
+package example01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Rectangle rect=new Rectangle();
+		Rectangle rectb=new Rectangle();
+		rect.setValues(3,4);
+		rectb.setValues(5,6);
+		
+		System.out.println("rect a: "+ rect.area());
+		System.out.println("rect b: "+ rectb.area());
+
+	}
+
+}
